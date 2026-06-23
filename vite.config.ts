1c2536adf35864722d35601ab19d1974727027b5
tommyLL123 <tommy.loo@utec.edu.pe>
2026-06-22 20:32:09 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://hackaton-20261-front-587720740455.us-east1.run.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
